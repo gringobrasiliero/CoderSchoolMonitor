119c0655522af0bee1804d8378243320ffc82cad
Nolan Byrnes <nolanehs@gmail.com>
2018-10-09 09:09:16 -0400

    Completed Project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Front Door Greeter
+
+This is a front door greeter that displays a logo from the Coder School, and when you connect your motion sensor to the GPIO pin, it will spin the logo in place, and play an audio recording.
+
+I made this project to be a door greeter for The Coder School, but of course you can exchange the logo and audio files for ones of your own. =}
+
+
+## Getting Started
+
+This works best on a Raspberry Pi. Make sure you have Pigame, connect your motion sensor to GPIO pin number 4.
+open and run "run.py" to start the program.
+
+Use the space bar to toggle between full screen and a resizable screen.
+
+### Prerequisites
+
+Python
+
+Pygame
+
+Raspberry PI
+
+
+## Authors
+
+* **Nolan Byrnes** - *Initial work* - [gringobrailiero](https://github.com/gringobrasiliero)
+
+
+## Acknowledgments
+
+* The Coder School
+
--- run.py
+++ run.py
@@ -1,58 +1,103 @@
-import pyglet
-from pyglet.window import mouse
+import sys
+import pygame
 from time import sleep
-
-fps = pyglet.clock.ClockDisplay()
-fullscreen = False
-
-window = pyglet.window.Window(width=800, height=800, caption='Resizable window', resizable=True)
-image = pyglet.resource.image('nyancat.jpg')
-imageTwo = pyglet.resource.image('black.jpg')
-logo = pyglet.resource.image('CoderSchoolLogo.png')
-logo.anchor_x = logo.width // 2
-logo.anchor_y = logo.height // 2
-music = pyglet.resource.media('01_02_Blank Space.wav')
-sprite = pyglet.sprite.Sprite(img=logo, x=0, y=0)
+from time import localtime
+from gpiozero import MotionSensor, LED
+from signal import pause
+WHITE = (255, 255, 255)
 
 
-@window.event
-def on_draw():
-    window.clear()
-    window.set_fullscreen(fullscreen=fullscreen, width=800, height=600)
+def main():
+    pygame.init()
+    size = [1000, 1000]
+    screen = pygame.display.set_mode(size)
+    screen.fill(WHITE)
+    pygame.display.update()
+    screen_rect = screen.get_rect()
+    pygame.display.set_caption("Welcome to the Coder School")
+    clock = pygame.time.Clock()
+    
+    ##########Image to display here##############
+    image_orig = pygame.image.load('CoderSchoolLogo.png').convert()
+    #############################################
+    image = image_orig.copy()
+    image_rect = image_orig.get_rect(center=screen_rect.center)
+    angle = 0
+    done = False
+    toggle = 0
+    
+    #############GPIO PIN######################
+    pir = MotionSensor(4)
+    ###########################################
+    
+    #############Music File To Play###########
+    pygame.mixer.music.load('welcome.wav')
+    ##########################################
+    play = 0
+    wait = 0    
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                # if the 'space' key is pressed
+                if event.key == pygame.K_SPACE:
+                    if toggle == 0:
+                        pygame.display.quit()
+                        pygame.display.init()
+                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                        screen.fill(WHITE)
+                        pygame.display.update()
+                        surface = pygame.display.get_surface()
+                        x = surface.get_width()/2
+                        image_rect.centerx = x
 
-    sprite.draw()
-    fps.draw()
+                        
+                        toggle = 1
+                    elif toggle == 1:
+                        pygame.display.quit()
+                        pygame.display.init()
+                        
+                        screen = pygame.display.set_mode((1200, 1200), pygame.RESIZABLE)
+                        screen.fill(WHITE)
+                        pygame.display.update()
+                        surface = pygame.display.get_surface()
+                        x = surface.get_width()/2
+                        image_rect.centerx = x
 
-def update(dt):
-    sprite.rotation += dt * 90
+                        toggle = 0
+        if pygame.mixer.music.get_busy() != True:
+            angle=0
+            image = pygame.transform.rotate(image_orig, angle)
+            image_rect = image.get_rect(center=image_rect.center)
+            screen.blit(image, image_rect)
+            pygame.display.flip()
+            if wait == 1:
+                sleep(30)
+                wait = 0
+            if wait == 0:    
+                if pir.motion_detected:
+                    pygame.mixer.music.play()
+                    print('Coder Detected. Greeting Coder.')
+                    print('Warning: Human decisions are removed from strategic defense. Skynet begins to learn at a geometric rate. It becomes self-aware at 2:14 a.m. Eastern time, August 29th.')
+                
+                
+        if pygame.mixer.music.get_busy() == True:
+            image = pygame.transform.rotate(image_orig, angle)
+            angle+=2.25
+            wait = 1
+            
+        
+        
+        image_rect = image.get_rect(center=image_rect.center)
+        screen.blit(image, image_rect)
+        pygame.display.flip()
+        clock.tick(60)
+        
 
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    if button == mouse.LEFT:
-        window.clear()
-        pyglet.clock.schedule_interval(update, 1 / 60.0)
-        print('The left mouse button was pressed.')
-        music.play()
+    pygame.quit()
+    sys.exit()
 
 
-
-        # logo.blit(x, y, 0)
-
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == pyglet.window.key.SPACE:
-        global fullscreen  # provides access to the global variable
-        fullscreen = not fullscreen  # swaps mode
-
-        if fullscreen:
-            # if it is in fullscreen mode, change the resolution to 800x600 pixels
-            window.set_fullscreen(fullscreen=fullscreen, width=800, height=600)
-        else:
-            # if not, change the resolution to 300x300 pixels
-            window.set_fullscreen(fullscreen=fullscreen, width=300, height=300)
-
-
-pyglet.app.run()
-
-
-
+if __name__ == "__main__":
+    main()
